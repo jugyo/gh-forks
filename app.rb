e733d013077c320c20b7b9e70d19232d6090f4c1
jugyo <jugyo.org@gmail.com>
2011-02-23 12:47:07 +0900

    improve the search

--- app.rb
+++ app.rb
@@ -18,12 +18,25 @@ end
 
 get '/:user/:repo' do
   @repo = params.values_at(:user, :repo).join("/")
-  unless json = REDIS.get(@repo)
-    url = "http://github.com/api/v2/json/repos/show/#{@repo}/network"
-    json = open(url).read
-    REDIS.set(@repo, json)
-    REDIS.expire(@repo, 60 * 60)
-  end
-  @data = JSON.parse(json)
+  @data = fetch("http://github.com/api/v2/json/repos/show/#{@repo}/network")
   haml :search
+end
+
+get '/:repo' do
+  @repo = params["repo"]
+  data = fetch("http://github.com/api/v2/json/repos/search/#{@repo}")
+  if data["repositories"][0]
+    redirect "/#{data["repositories"][0].values_at("owner", "name").join("/")}"
+  else
+    haml :repository_not_found
+  end
+end
+
+def fetch(url, expire = 60 * 60)
+  unless json = REDIS.get(url)
+    json = open(url).read
+    REDIS.set(url, json)
+    REDIS.expire(url, 60 * 60)
+  end
+  JSON.parse(json)
 end
